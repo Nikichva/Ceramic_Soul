d145df71be6bec3540d341247a7517fecd230400
Nikichva <niki.22.12.00@gmail.com>
2025-08-04 18:33:14 +0200

    added slider

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+// core version + navigation, pagination modules:
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 import "/src/styles/tailwind.css";
 import "/src/styles/fontello.css";
 import "/src/styles/globals.css";
@@ -12,4 +20,29 @@ const sectionList = ["header.html", "promo.html", "footer.html"];
     const html = await res.text();
     app.insertAdjacentHTML("beforeend", html);
   }
+  // init Swiper:
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clikable: true,
+    },
+    navigation: {
+      nextEl: ".icon-right-open",
+      prevEl: ".icon-left-open",
+    },
+    breakpoints: {
+      // when window width is >= 1200px
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 5,
+      },
+      1920: {
+        spaceBetween: 35,
+        slidesPerView: 3,
+      },
+    },
+    modules: [Navigation, Pagination],
+  });
 })();
