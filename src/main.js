2da5eea269a612f98b4f866644a43cf53d7b9271
Nikichva <niki.22.12.00@gmail.com>
2025-08-07 23:44:53 +0200

    added catalog page

--- src/main.js
+++ src/main.js
@@ -1,20 +1,27 @@
-// core version + navigation, pagination modules:
+// src/main.js
+import "/src/styles/tailwind.css";
+import "/src/styles/fontello.css";
+import "/src/styles/globals.css";
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
-// import Swiper and modules styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-import "/src/styles/tailwind.css";
-import "/src/styles/fontello.css";
-import "/src/styles/globals.css";
+const pageMap = {
+  "/": "promo.html",
+  "/index.html": "promo.html",
+  "/catalog.html": "catalog.html",
+  "/blog.html": "blog.html",
+  "/about.html": "about.html",
+};
 
-const sectionList = ["header.html", "promo.html", "footer.html"];
+// pick fragment or default to promo.html
+const bodySection = pageMap[window.location.pathname] || "promo.html";
+const sectionList = ["header.html", bodySection, "footer.html"];
 
 (async () => {
   const app = document.getElementById("app");
-
   for (const file of sectionList) {
     const res = await fetch(`/src/sections/${file}`);
     const html = await res.text();
@@ -71,4 +78,26 @@ const sectionList = ["header.html", "promo.html", "footer.html"];
     },
     modules: [Navigation, Pagination],
   });
+
+  try {
+    const tabs = document.querySelectorAll(".catalog__tab");
+    const contents = document.querySelectorAll(".catalog__content-item");
+
+    tabs.forEach((tab, index) => {
+      tab.addEventListener("click", () => {
+        // Удаляем активный класс у всех табов и контента
+        tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
+        contents.forEach((c) => (c.style.display = "none"));
+
+        // Добавляем активный класс к нажатому табу и показываем соответствующий контент
+        tab.classList.add("catalog__tab_active");
+        contents[index].style.display = "flex";
+      });
+    });
+
+    // Показываем первый контент при загрузке
+    contents.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
+  } catch (e) {}
+
+  // Обратите внимание, что значение block (в двух местах) можно спокойно поменять на flex, если вам это необходимо
 })();
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        catalog: resolve(__dirname, "catalog.html"),
+        blog: resolve(__dirname, "blog.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
 });
